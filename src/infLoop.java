5c113d70e4a15690a157b247728f3fca770e7836
kardelio <kardeliogithub@outlook.com>
2018-10-15 10:07:45 +0100

    Test

---
+++ src/infLoop.java
@@ -0,0 +1,11 @@
+public class infLoop {
+
+    public static void main(String[] args)
+    {
+        while (true)
+        {
+            System.out.println("Yes");
+        }
+    }
+
+}
--- src/loops.java
+++ src/loops.java
@@ -96,6 +96,54 @@ public class loops {
          */
         endOfSection();
 
+        // =================================
+        // =================================
+        // =================================
+
+        startOfSection(4);
+        /**
+         * Section 4 ......................
+         */
+
+        /**
+         * while is slightly different as it uses just a simple statement of truth to decide if it should continue..
+         *
+         * while (STATEMENT HERE IS EITHER TRUE OR FALSE)
+         * - if its true the code body will run
+         * - if its not then the code body will NOT run
+         */
+
+        int j = 0;
+        while (j < 10)
+        {
+            System.out.println(j);
+            j++;
+        }
+
+        /**
+         * Q. 1
+         * What would happen if i ran this?
+         *
+         * while(true)
+         * {
+         *  System.out.println("Yes");
+         * }
+         *
+         *
+         * Q. 2
+         * What would happen if i ran this?
+         *
+         * String myStr = "hello";
+         * while(myStr == "Goodbye")
+         * {
+         *  System.out.println("You said goodbye");
+         * }
+         */
+
+        /**
+         * END Section 4 ......................
+         */
+        endOfSection();
     }
 
 }
